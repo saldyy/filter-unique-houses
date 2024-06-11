5da6bb4e3de95d09e95f42caf972e142d085bc2c
Phillip Nguyen <png9981@gmail.com>
2024-06-11 22:19:35 +0700

    feat: update logic validating csv file

--- src/shared/pipes/file-transform.pipes.ts
+++ src/shared/pipes/file-transform.pipes.ts
@@ -13,6 +13,9 @@ export class FileTransformPipe
   transform(value: Express.Multer.File, metadata: ArgumentMetadata) {
     const csvString = Buffer.from(value.buffer).toString();
     const rows = csvString.split('\n');
+
+    this.validateHeader(rows[0]);
+
     const result: HouseData[] = [];
     for (let i = 1; i < rows.length; i++) {
       if (!rows[i]) {
@@ -20,9 +23,17 @@ export class FileTransformPipe
       }
       const row = rows[i];
       const [id, address] = row.split(',');
+      if (!id || !address) {
+        continue;
+      }
       result.push({ id: parseInt(id), address });
     }
 
     return result;
+  }
+  private validateHeader(header: string) {
+    if (header !== 'houseId,houseAddress') {
+      throw new BadRequestException('CSV headerk');
+    }
   }
 }
